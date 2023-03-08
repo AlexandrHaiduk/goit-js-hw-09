9f20db69c7744879da2dfd4499372bfb6161913b
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-03-08 18:26:50 +0200

    added dates compare

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,11 +6,13 @@ const datePicker = document.querySelector('input#datetime-picker')
 
 const startBtn = document.querySelector('button[data-start]')
 
+const currentDate = new Date()
+
 startBtn.disabled = true
 
-document.body.addEventListener("click", () => {
-    Notiflix.Notify.failure('Please choose a date in the future')
-})
+// document.body.addEventListener("click", () => {
+//     Notiflix.Notify.failure('Please choose a date in the future')
+// })
 
 const options = {
     enableTime: true,
@@ -18,7 +20,15 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
+
+        if(selectedDates[0] < currentDate) {
+            Notiflix.Notify.failure('Please choose a date in the future')
+        }
       console.log(selectedDates[0]);
+      console.log(`currentDate: ${currentDate}` );
+      console.log(selectedDates[0] > currentDate);
+
+
     },
   };
 
